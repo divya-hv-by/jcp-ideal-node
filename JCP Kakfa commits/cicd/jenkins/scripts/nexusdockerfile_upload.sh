358a23be778e6ff5f9dcf856e27b1051382a76df
Deepak Yadav <deepakyadav@in2-1029173mbp.local>
2022-03-30 18:04:53 +0530

    Added cicd package

---
+++ JCP Kakfa commits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
@@ -0,0 +1,20 @@
+PACKAGE_NUMBER=$1
+#USER_PWD=Dev-readonly-new:$password
+USER_PWD=dev-readonly:$password
+NEXUS_URL=https://nexus.yantriks.in/nexus/service/local/repositories
+JFROG_URL=https://jdasoftware.jfrog.io/jdasoftware
+JFROG_USER_PWD=ecom-ci:$jfrogpassword
+if [ "$#" -gt 1 ]
+then
+        REPO=$2
+		JFROG_REPO=libs-release-local
+else
+        REPO=release-candidate
+		JFROG_REPO=libs-snapshot-local
+fi
+
+
+curl -v -u ${USER_PWD} --upload-file $WORKSPACE/commits-weight-based/cicd/jenkins/scripts/startup.sh ${NEXUS_URL}/${REPO}/content/com/yantriks/poc/poc-sourcing-extension/${PACKAGE_NUMBER}/startup.sh
+curl -v -u ${USER_PWD} --upload-file $WORKSPACE/commits-weight-based/cicd/jenkins/scripts/Dockerfile ${NEXUS_URL}/${REPO}/content/com/yantriks/poc/poc-sourcing-extension/${PACKAGE_NUMBER}/Dockerfile
+
+
---
+++ JCP Kakfa commits/cicd/jenkins/scripts/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Start common-kafka-consumer Server
+#cd /configmap && java $JAVA_OPTS $JVM_MEM_ARGS $JVM_PERF_ARGS -jar /opt/api/microservice-sourcing-tsc-optimizer-app.jar
+cd /configmap && java $JAVA_OPTS $JVM_MEM_ARGS $JVM_PERF_ARGS -jar /opt/api/commits-exts.jar
+
