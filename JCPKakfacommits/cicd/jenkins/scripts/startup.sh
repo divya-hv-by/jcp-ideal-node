b480cca2a651277ee6f787bf0b067169e31a97ca
baji-shaik-by <79303808+baji-shaik-by@users.noreply.github.com>
2022-04-04 18:55:30 +0530

    Update startup.sh

--- JCPKakfacommits/cicd/jenkins/scripts/startup.sh
+++ JCPKakfacommits/cicd/jenkins/scripts/startup.sh
@@ -2,5 +2,5 @@
 
 # Start common-kafka-consumer Server
 #cd /configmap && java $JAVA_OPTS $JVM_MEM_ARGS $JVM_PERF_ARGS -jar /opt/api/microservice-sourcing-tsc-optimizer-app.jar
-cd /configmap && java $JAVA_OPTS $JVM_MEM_ARGS $JVM_PERF_ARGS -jar /opt/api/commits-exts.jar
+cd /configmap && java $JAVA_OPTS $JVM_MEM_ARGS $JVM_PERF_ARGS -jar /opt/api/event.jar
 
