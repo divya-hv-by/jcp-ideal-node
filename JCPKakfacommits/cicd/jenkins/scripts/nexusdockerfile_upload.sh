5566423cf05e493c065f5569ad975822579d5d10
baji-shaik-by <79303808+baji-shaik-by@users.noreply.github.com>
2022-04-05 14:09:22 +0530

    Update nexusdockerfile_upload.sh

--- JCPKakfacommits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
+++ JCPKakfacommits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
@@ -10,3 +10,6 @@ else
 fi
 curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/startup.sh ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/jcp-ideal/${PACKAGE_NUMBER}/startup.sh
 curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/Dockerfile ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/jcp-ideal/${PACKAGE_NUMBER}/Dockerfile
+
+
+
