f3292a81a3cc9e8cf4ddd29a743e87bfc6f4c285
baji-shaik-by <79303808+baji-shaik-by@users.noreply.github.com>
2022-04-05 11:14:50 +0530

    Update nexusdockerfile_upload.sh

--- JCPKakfacommits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
+++ JCPKakfacommits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
@@ -2,12 +2,11 @@ PACKAGE_NUMBER=$1
 #USER_PWD=Dev-readonly-new:$password
 USER_PWD=dev-readonly:$password
 NEXUS_URL=https://nexus.yantriks.in/nexus/service/local/repositories
-if [ "$#" -gt 1 ]; 
+if [ "$#" -gt 1 ]
 then
         REPO=$2
 else
         REPO=release-candidate
 fi
-
 curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/startup.sh ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/jcp-ideal/${PACKAGE_NUMBER}/startup.sh
 curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/Dockerfile ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/jcp-ideal/${PACKAGE_NUMBER}/Dockerfile
