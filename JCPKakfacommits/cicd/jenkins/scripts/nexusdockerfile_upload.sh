10a041fb593194e78f73c6b4c7307726557d24d5
baji-shaik-by <79303808+baji-shaik-by@users.noreply.github.com>
2022-05-23 14:04:07 +0530

    Update nexusdockerfile_upload.sh

--- JCPKakfacommits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
+++ JCPKakfacommits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
@@ -9,6 +9,6 @@ else
 fi
 
 
-curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/startup.sh ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/events/extensions/ideal-node/${PACKAGE_NUMBER}/startup.sh
-curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/Dockerfile ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/events/extensions/ideal-node/${PACKAGE_NUMBER}/Dockerfile
+curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/startup.sh ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/events/extensions/ideal-node-new/${PACKAGE_NUMBER}/startup.sh
+curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/Dockerfile ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/events/extensions/ideal-node-new/${PACKAGE_NUMBER}/Dockerfile
 
