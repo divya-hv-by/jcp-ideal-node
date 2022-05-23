b6d0e4f4285c0bb3cf33862805b3806680c40d39
baji-shaik-by <79303808+baji-shaik-by@users.noreply.github.com>
2022-05-23 18:21:06 +0530

    Update nexusdockerfile_upload.sh

--- JCPKakfacommits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
+++ JCPKakfacommits/cicd/jenkins/scripts/nexusdockerfile_upload.sh
@@ -3,12 +3,11 @@ PACKAGE_NUMBER=$1
 USER_PWD=dev-readonly:$password
 NEXUS_URL=https://nexus.yantriks.in/nexus/service/local/repositories
 if [ "$#" -gt 1 ]
+then
         REPO=$2
 else
         REPO=release-candidate
 fi
-
-
-curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/startup.sh ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/events/extensions/ideal-node-new/${PACKAGE_NUMBER}/startup.sh
-curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/Dockerfile ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/events/extensions/ideal-node-new/${PACKAGE_NUMBER}/Dockerfile
+curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/startup.sh ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/jcp-ideal-new/${PACKAGE_NUMBER}/startup.sh
+curl -v -u ${USER_PWD} --upload-file $WORKSPACE/JCPKakfacommits/cicd/jenkins/scripts/Dockerfile ${NEXUS_URL}/${REPO}/content/com/yantriks/JCPKakfacommits/jcp-ideal-new/${PACKAGE_NUMBER}/Dockerfile
 
